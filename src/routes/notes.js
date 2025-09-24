35b29d1c02db0042bce6999a5d322170ddeb4ac3
Nico Jansson <janssnic@arcada.fi>
2025-09-24 12:45:08 +0300

    get, post, patch, delete

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -10,10 +10,10 @@ const prisma = new PrismaClient()
 
 //laddar in i en board
 //får alla notes som hör till den med id
-router.get('/:id', async (req, res) => {
+router.get('/:boardid', async (req, res) => {
     try {
         const notes = await prisma.notes.findMany({
-            where: { board_id: parseInt(req.params.id, 10) }
+            where: { board_id: parseInt(req.params.boardid, 10) }
         })
         res.json(notes)
     } catch (error) {
@@ -27,19 +27,51 @@ router.get('/:id', async (req, res) => {
 //body
 router.post('/:id', async (req, res) => {
     try {
-        const newNotes = await prisma.notes.create({
+        const note = await prisma.notes.create({
             data: {
-                content: req.body.text,
+                content: req.body.content,
                 x_loc: req.body.x_loc,
                 y_loc: req.body.y_loc,
-                board_id: req.params.id
-            }
-        })
-        res.json({newNotes})
-
+                board_id: parseInt(req.params.id)
+            },
+        });
+        res.json(note);
     } catch (error) {
-        res.status(500).send({ msg: "Error", error })
+        console.error(error);
+        res.status(500).json({ msg: "Error creating note" });
     }
 
 })
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const updatedNote = await prisma.notes.update({
+            where: { id: parseInt(req.params.id) },
+            data: {
+                content: req.body.content,
+                x_loc: req.body.x_loc,
+                y_loc: req.body.y_loc,
+            },
+        });
+        res.json(updatedNote);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Error updating note" });
+    }
+
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedNote = await prisma.notes.delete({
+            where: { id: parseInt(req.params.id) },
+        });
+        res.json(deletedNote);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Error deleting note" });
+    }
+
+})
+
 module.exports = router
